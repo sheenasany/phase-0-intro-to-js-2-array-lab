caca111fd30510930848bf6ec5fe8dd05cc6f4e5
Sheena Sang <sheenasany@gmail.com>
2022-06-13 15:42:06 -0400

    commit tree 12f640ba71690d184da0dcee72f90c0bb057a25c

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newcats = [...cats, "Broom"];
+    return (newcats);
+}
+
+function prependCat(name) {
+    const newcats = ["Arnold", ...cats];
+    return (newcats);
+}
+
+function removeLastCat() {
+    return (cats.slice(0, cats.length - 1));
+}
+
+function removeFirstCat() {
+    return (cats.slice(1));
+}
