ad795cd1d2ef929f5f6113050cd2086d2b3b67b0
Sheena Sang <sheenasany@gmail.com>
2023-08-28 12:18:46 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+const destructivelyAppendCat = (name) => {
+    return cats.push(name)
+}
+
+const destructivelyPrependCat = (name) => {
+    return cats.unshift(name)
+}
+
+const destructivelyRemoveLastCat = () => {
+    return cats.pop()
+}
+
+const destructivelyRemoveFirstCat = () => {
+    return cats.shift()
+}
+
+const appendCat = (name) => {
+    return [...cats, name]
+}
+
+const prependCat = (name) => {
+    return [name, ...cats]
+}
+
+const removeLastCat = () => {
+    return cats.slice(0, cats.length - 1)
+}
+
+const removeFirstCat = () => {
+    return cats.slice(1)
+}
